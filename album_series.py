ec7ba91009658352666d65e25d16d85ea27d1f15
H. Boyd <harrisonboyd400@gmail.com>
2022-08-30 22:51:33 +0000

    Fix license

--- album_series.py
+++ album_series.py
@@ -1,44 +1,34 @@
 # -*- coding: utf-8 -*-
 
-# This is the Sort Multivalue Tags plugin for MusicBrainz Picard.
+# This is the Album Series plugin for MusicBrainz Picard.
 # Copyright © 2022 Harrison Boyd
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>
 
 PLUGIN_NAME = 'Album Series'
 PLUGIN_AUTHOR = 'Harrison Boyd'
 PLUGIN_LICENSE = 'GPL-2.0-or-later'
-PLUGIN_LICENSE_URL = 'https://www.gnu.org/licenses/gpl-2.0.html'
-PLUGIN_DESCRIPTION = '''Adds the tag series_name which contains the name of the series relation of a release '''
+PLUGIN_LICENSE_URL = 'https://www.gnu.org/licenses/gpl-3.0.html'
+PLUGIN_DESCRIPTION = '''Adds the tag series_name which contains the name of the series relation of a release'''
 PLUGIN_VERSION = "0.5"
 PLUGIN_API_VERSIONS = ["2.0"]
 
-import os
-import sys
-from enum import IntEnum
-from os import path
 
-import picard.formats
-from picard import config
-from picard import file as picardFile
-from picard import log
-from picard.album import Album
 from picard.metadata import register_album_metadata_processor
-from picard.formats.vorbis import FLACFile
-from picard.script import register_script_function
-from picard.ui.itemviews import BaseAction, register_album_action
 from picard.webservice import WebService
 from picard.webservice.api_helpers import MBAPIHelper
 from picard import plugin
-import time
 
 webservice = WebService()
 mb_api = MBAPIHelper(webservice)
